f7b7faa7d62d723b7a165459b1e97ac2faa5335e
martyrk <admin@martyrk.design>
2021-10-14 12:32:00 -0400

    improve login page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -14,16 +14,17 @@ const Login = () => {
         style={{ maxWidth: "50%" }}
         onSubmit={handleSubmit}
       >
+        <h1 className="mb-3">Login</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="placeholder@example.com" />
           <Form.Text className="text-muted">
             We will never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="••••••••" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
