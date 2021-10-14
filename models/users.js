df992d704cf6b1a6c8253b9788ccbe12699c5ff0
martyrk <admin@martyrk.design>
2021-10-14 10:54:38 -0400

    add login route

--- models/users.js
+++ models/users.js
@@ -1,56 +1,29 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const codeSchema = new mongoose.Schema({
-  inviteCode: {
+const userSchema = new mongoose.Schema({
+  email: {
     type: String,
-    required: [true, "Please specify an invite code."],
-    unique: [true, "This invite code already exists."],
+    unique: [true, "This email address already exists, please choose another."],
+    required: [true, "Please specify an email address."],
+  },
+  password: {
+    type: String,
+    required: [true, "Please specify a password."],
     select: false,
   },
-  //   alias: {
-  //     type: String,
-  //     required: [true, "Please specify an alias for the invite code."],
-  //     unique: [true, "This alias already exists."],
-  //   },
-  //   email: {
-  //     type: String,
-  //     unique: [true, "This email address already exists, please choose another."],
-  //   },
-  //   identifier: {
-  //     type: String,
-  //     required: true,
-  //     unique: true,
-  //   },
-  //   attendance: {
-  //     type: String,
-  //     required: [true, "Please specify the attendance type."],
-  //     enum: ["virtual", "in-person"],
-  //   },
-  //   invitedGuests: Array,
-  //   confirmedGuests: Array,
-  //   rsvp: {
-  //     type: Boolean,
-  //     default: false,
-  //   },
-  //   song: String,
-  //   role: {
-  //     type: String,
-  //     enum: ["guest", "admin"],
-  //     default: "guest",
-  //   },
 });
 
-codeSchema.pre("save", async function (next) {
-  if (!this.isModified("inviteCode")) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-  this.inviteCode = await bcrypt.hash(this.inviteCode, 12);
+  this.password = await bcrypt.hash(this.password, 12);
 
   return next();
 });
 
-codeSchema.methods.comparePassword = async (candidatePassword, userPassword) =>
+userSchema.methods.comparePassword = async (candidatePassword, userPassword) =>
   bcrypt.compare(candidatePassword, userPassword);
 
 // prevents mongoose from recompiling the model
-export default mongoose.models.Code || mongoose.model("Code", codeSchema);
+export default mongoose.models.User || mongoose.model("User", userSchema);
--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,25 +1,26 @@
 import nc from "next-connect";
 
 import dbConnect from "../../lib/dbConnect";
-import Code from "../../models/codes"; // - generic model, i.e. user
+import User from "../../models/users";
 
 const handler = nc();
 
 handler.use(dbConnect).post(async (req, res) => {
-  // - check to ensure an email & password were entered
+  const { email, password } = req.body;
 
-  const user = await Code.findOne({
-    email: req.body.email,
-  }).select("+inviteCode"); // - make more generic, e.g. password
+  if (!email || !password) {
+    console.log("Missing credentials, please try again.");
+  }
+
+  const user = await User.findOne({
+    email,
+  }).select("+password");
 
   if (!user) {
     console.log("Incorrect email or password, please try again.");
   }
 
-  if (
-    user &&
-    (await user.comparePassword(req.body.inviteCode, code.inviteCode))
-  ) {
+  if (user && (await user.comparePassword(password, user.password))) {
     // - send jwt?
   }
 });
