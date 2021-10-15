ea84e64ab769494e176e97a4f787eb503844df99
martyrk <admin@martyrk.design>
2021-10-15 13:33:08 -0400

    rework routeguard for prrivate routes

--- components/RouteGuard.js
+++ components/RouteGuard.js
@@ -1,64 +1,22 @@
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-
 import { useUser } from "contexts/UserContext";
 
-function RouteGuard({ children }) {
-  const router = useRouter();
+const RouteGuard = ({ children }) => {
   const { user } = useUser();
+  const router = useRouter();
   const [authorized, setAuthorized] = useState(false);
 
   useEffect(() => {
-    // on initial load - run auth check
-    authCheck(router.asPath);
-
-    // // on route change start - hide page content by setting authorized to false
-    // const hideContent = () => setAuthorized(false);
-    // router.events.on("routeChangeStart", hideContent);
-
-    // on route change complete - run auth check
-    router.events.on("routeChangeComplete", verifyAuth);
-
-    // // unsubscribe from events in useEffect return function
-    return () => {
-      //   router.events.off("routeChangeStart", hideContent);
-      router.events.off("routeChangeComplete", authCheck);
-    };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function authCheck(url) {
-    // redirect to login page if accessing a private page and not logged in
-    const publicPaths = ["/auth/login", "/auth/signup"];
-    const path = url.split("?")[0];
-
-    if (!user && !publicPaths.includes(path)) {
-      setAuthorized(false);
-      router.push({
-        pathname: "/auth/login",
-      });
+    if (!user) {
+      // - toast message?
+      router.push("/auth/login");
     } else {
       setAuthorized(true);
     }
-  }
-
-  function verifyAuth(url) {
-    const publicPaths = ["/auth/login", "/auth/signup"];
-    const path = url.split("?")[0];
-    console.log(path);
-    console.log(user);
-
-    if (!user && !publicPaths.includes(path)) {
-      console.log("i'm not here!");
-      setAuthorized(false);
-    } else {
-      console.log("i'm here!");
-      setAuthorized(true);
-    }
-  }
+  }, [user, router]);
 
   return authorized && children;
-}
+};
 
 export default RouteGuard;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-
-import { useUser } from "../contexts/UserContext";
+import RouteGuard from "components/RouteGuard";
+import { useUser } from "contexts/UserContext";
 
 const Home = () => {
   const { user } = useUser();
-  const router = useRouter();
-  const [authorized, setAuthorized] = useState(false);
 
-  useEffect(() => {
-    if (!user) {
-      // - toast message?
-      router.push("/auth/login");
-    } else {
-      setAuthorized(true);
-    }
-  }, [user, router]);
-
-  return authorized && <h1>Hello {user.email}!</h1>;
+  return (
+    <RouteGuard>
+      <h1>Hello {user?.email}!</h1>
+    </RouteGuard>
+  );
 };
 
 export default Home;
