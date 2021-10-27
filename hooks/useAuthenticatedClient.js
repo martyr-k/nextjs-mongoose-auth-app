5432e7b2bd7f532c9b6324c9a21b72d9d8c08d7f
martyrk <admin@martyrk.design>
2021-10-27 15:55:46 -0400

    fix issue where jwt would not refresh prior to expiry

--- hooks/useAuthenticatedClient.js
+++ hooks/useAuthenticatedClient.js
@@ -1,10 +1,12 @@
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { useAuthentication } from "contexts/AuthenticationContext";
+
+let count = 0;
 
 const fetcher = (url, token) => {
   return axios
@@ -21,18 +23,22 @@ const fetcher = (url, token) => {
 function useAuthenticatedClient(redirectTo, ...roles) {
   const router = useRouter();
   const { token } = useAuthentication();
+  const { mutate } = useSWRConfig();
 
   const { data, error } = useSWR(token && ["/api/users", token], fetcher, {
     errorRetryCount: 0,
     refreshWhenHidden: true,
     onError: (error) => {
-      if (error.response.data === "jwt expired") {
-        // mutate auth refresh?
-        console.log("useAuthenticatedClient:", error.response);
-      } else {
+      if (error.response.data === "jwt expired" && count === 0) {
+        count = 1;
+        mutate("/api/auth/refresh-token");
+      } else if (error?.response.data !== "jwt expired") {
         console.log("useAuthenticatedClient:", error);
         toast.error(error.response.data);
       }
+    },
+    onSuccess: () => {
+      count = 0;
     },
   });
   const isLoading = !error && !data;
